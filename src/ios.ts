4f9af64da9f8db8b63e1f8765e909f2621a881b4
watanabeyu <fight.ippatu5648@gmail.com>
2022-08-04 19:27:20 +0900

    fix android

--- src/ios.ts
+++ src/ios.ts
@@ -1,15 +1,20 @@
-export const getIOSVersion = async (storeURL: string = '', country: string = 'jp'): Promise<string> => {
+export const getIOSVersion = async (storeURL = '', country = 'jp') => {
   const appID = storeURL.match(/.+id([0-9]+)\??/);
 
   if (!appID) {
     throw new Error('iosStoreURL is invalid.');
   }
 
-  const response = await fetch(`https://itunes.apple.com/lookup?id=${appID[1]}&country=${country}&${new Date().getTime()}`, {
-    headers: {
-      'cache-control': 'no-cache',
-    },
-  })
+  const response = await fetch(
+    `https://itunes.apple.com/lookup?id=${
+      appID[1]
+    }&country=${country}&${new Date().getTime()}`,
+    {
+      headers: {
+        'cache-control': 'no-cache',
+      },
+    }
+  )
     .then((r) => r.text())
     .then((r) => JSON.parse(r));
 
@@ -17,5 +22,5 @@ export const getIOSVersion = async (storeURL: string = '', country: string = 'jp
     throw new Error(`appID(${appID[1]}) is not released.`);
   }
 
-  return response.results[0].version;
+  return response.results[0].version as string;
 };
