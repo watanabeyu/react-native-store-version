d5b9e32ee6b4035adc5cde468c22dae3c22b8926
watanabeyu <fight.ippatu5648@gmail.com>
2022-08-04 19:07:40 +0900

    create individual test

---
+++ __tests__/android.test.ts
@@ -0,0 +1,19 @@
+import { getAndroidVersion } from '../src/android';
+
+require('jest-fetch-mock');
+
+describe('android', () => {
+  beforeEach(() => {
+    jest.mock('react-native', () => ({ Platform: { os: 'android' } }));
+  });
+
+  it('get version', async () => {
+    try {
+      await getAndroidVersion(
+        'https://play.google.com/store/apps/details?id=com.github.android'
+      );
+    } catch (e) {
+      expect(e).toHaveProperty('message');
+    }
+  });
+});
